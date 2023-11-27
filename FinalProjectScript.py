8ec01ad7538fe765fd412ee32a7bf336c9233312
bfribush <bfribush@centegix.com>
2023-11-27 16:34:18 -0500

    Update FinalProjectScript.py

--- FinalProjectScript.py
+++ FinalProjectScript.py
@@ -14,7 +14,7 @@ import matplotlib.image as mpimg
 
 
 
-print(bnn.available_params(bnn.NETWORK_LFCW1A1))
+print(bnn.available_params(bnn.NETWORK_LFCW1A2))
 hw_classifier = bnn.LfcClassifier(bnn.NETWORK_LFCW1A2,"mnist",bnn.RUNTIME_HW)
 sw_classifier = bnn.LfcClassifier(bnn.NETWORK_LFCW1A2,"mnist",bnn.RUNTIME_SW)
 print(hw_classifier.classes)
@@ -23,7 +23,39 @@ print(hw_classifier.classes)
 cap = cv2.VideoCapture(-1) 
 _ , cv2_im = cap.read()
 cv2_im = cv2.cvtColor(cv2_im,cv2.COLOR_BGR2RGB)
-img = PIL_Image.fromarray(cv2_im).convert("L") 
+
+cv2.imwrite('Shadowed.jpg', cv2_im)
+
+yes = cv2.imread('Shadowed.jpg', -1)
+
+rgb_planes = cv2.split(yes)
+
+result_planes = []
+result_norm_planes = []
+for plane in rgb_planes:
+    dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+    bg_img = cv2.medianBlur(dilated_img, 21)
+    diff_img = 255 - cv2.absdiff(plane, bg_img)
+    norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    result_planes.append(diff_img)
+    result_norm_planes.append(norm_img)
+    
+result = cv2.merge(result_planes)
+result_norm = cv2.merge(result_norm_planes)
+
+cv2.imwrite('shadows_out.jpg', result)
+cv2.imwrite('shadows_out_norm.jpg', result_norm)
+
+
+
+
+
+orig_img_path = '/home/xilinx/jupyter_notebooks/bnn/shadows_out_norm.jpg'
+img = PIL_Image.open(orig_img_path).convert("L")     
+                   
+
+
+#img = PIL_Image.fromarray(cv2_im).convert("L") 
 
 
 #original captured image
@@ -63,8 +95,8 @@ img_data=np.asarray(background)
 img_data = img_data[:,:,0]  
 imageio.imwrite('SMALLimg_webcam_mnist.png', img_data) 
 nope = mpimg.imread('SMALLimg_webcam_mnist.png')
-plt.imshow(nope)
-plt.show()
+#plt.imshow(nope)
+#plt.show()
 
 
 
@@ -82,12 +114,18 @@ smallimg = smallimg.rotate(0)
 data_image = array('B')  
   
 pixel = smallimg.load()  
+#for x in range(0,28):  
+#    for y in range(0,28):  
+#        if(pixel[y,x] >= 125):  
+#            data_image.append(255)  
+#        else:  
+#            data_image.append(1)  
+
 for x in range(0,28):  
-    for y in range(0,28):  
-        if(pixel[y,x] >= 100):  
-            data_image.append(255)  
-        else:  
-            data_image.append(1)  
+    for y in range(0,28): 
+        data_image.append(pixel[y,x])
+
+
           
 # Setting up the header of the MNIST format file - Required as the hardware is designed for MNIST dataset         
 hexval = "{0:#0{1}x}".format(1,6)  
@@ -101,22 +139,17 @@ header[3] = 3 # Changing MSB for image data (0x00000803)
 pure = data_image
 pure = np.frombuffer(data_image, dtype=np.uint8)
 pure = pure.reshape(1,-1)
-
 pure = pure.reshape(28,28)
-
-
 cv2.imwrite('FLIPPEDSMALLimg_webcam_mnist.png', pure) 
-
 data_image = header + data_image  
 output_file = open('/home/xilinx/img_webcam_mnist_processed', 'wb')  
 data_image.tofile(output_file)  
 output_file.close()   
 
 
-
 crack = mpimg.imread('FLIPPEDSMALLimg_webcam_mnist.png')
-plt.imshow(crack)
-plt.show()
+#plt.imshow(crack)
+#plt.show()
 
 
 
@@ -124,7 +157,11 @@ class_out = hw_classifier.classify_mnist("/home/xilinx/img_webcam_mnist_processe
 print("Class number: {0}".format(class_out))
 print("Class name: {0}".format(hw_classifier.class_name(class_out)))
 
-
 cap.release()
+
+#plt.imshow(nope)
+#plt.show()
+plt.imshow(crack)
+plt.show()
 img
 
